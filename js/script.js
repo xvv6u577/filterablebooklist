41dab52ec6fe00086bd255e6b97e4512aeda80c1
caster8013 <warley8013@gmail.com>
2016-08-03 16:03:57 +0800

    rewrite

--- js/script.js
+++ js/script.js
@@ -1,116 +1,73 @@
-var BookListTable = React.createClass({
-	getInitialState: function() {
-    		return { bookLists:[] };
-  	},
-	componentDidMount: function() {
-		this.loadBookLists();
-		{/*setInterval(this.loadBookLists,this.props.pollInterval);*/}
-	},
-	loadBookLists:function(){	
-		$.ajax({
-			url:this.props.url+"?q="+this.props.filterText,
-			dataType:"jsonp",
-			success:function(data){
-				this.setState({bookLists:data.books});
-				console.log(data.books);
-			}.bind(this),
-			error:function(xhr,sta,e){
-				console.log(xhr.toString()+" "+sta+" "+e.toString());
-			}
-		});
-	},
-	render: function() {
-		let books = this.state.bookLists.filter((book)=>{
-			if(this.props.betterThan8 && book.rating.average <8.0)
-				return false;
-			return true;    	
-		}).map((book) => {
-			return(
-			<li key={book.id}>
-				<img src={book.image}/>
-				<p>{book.title}</p>
-				<p>{book.author}</p>
-				<p>{book.rating.average}</p>
-			</li>
-			);
-		});
-	return (
-	  <div className="booklisttable">
-		{books}
-	  </div>
-	);
-	}
+var FooSon = React.createClass({
+  render:function(){
+    return (
+      <div>
+        <div>text:{this.props.text} checkbox:{this.props.checkbox.toString()}</div>
+        <ul>{this.props.books}</ul>
+      </div>
+    );
+  }
 });
-
-var SearchBar = React.createClass({
-	handleChange: function() {
-		this.props.onUserInput(
-		  this.refs.filterTextInput.value,
-		  this.refs.betterThan8Input.checked
-		);
-	},
-	render: function() {
-		return (
-			<form>
-			<input
-			  type="text"
-			  placeholder="Search..."
-			  value={this.props.filterText}
-			  ref="filterTextInput"
-			  onChange={this.handleChange}
-			/>
-			<p>
-			  <input
-				type="checkbox"
-				checked={this.props.betterThan8}
-				ref="betterThan8Input"
-				onChange={this.handleChange}
-			  />
-			  {' '}
-			  Only show books more than 8 points.
-			</p>
-			</form>
-		);
-	}
+var ImageComponent = React.createClass({
+  render:function(){
+    return (
+      <div className="logo">
+        <img src="https://img3.doubanio.com/f/shire/8308f83ca66946299fc80efb1f10ea21f99ec2a5/pics/nav/lg_main_a11_1.png"/>
+      </div>
+    );
+  }
 });
-
-var FilterableBookListTable = React.createClass({
-	getInitialState: function() {
-		return {
-		  filterText: '',
-		  betterThan8: false
-		};
-	},
-	
-	handleUserInput: function(filterText, betterThan8) {
-		this.setState({
-			filterText: filterText,
-			betterThan8: betterThan8
-		});
-	},
-	
-	render: function() {
-		return (
-			<div className="booklisttable">
-				<img src="http://easyread.ph.126.net/xeO9di9z_9onqo8F1Bplqg==/7916659641848707440.png"/>
-				<hr />
-				<SearchBar
-				  filterText={this.state.filterText}
-				  betterThan8={this.state.betterThan8}
-				  onUserInput={this.handleUserInput}
-				/>
-				<BookListTable
-				  url={this.props.url}
-				  pollInterval={this.props.pollInterval}
-				  filterText={this.state.filterText}
-				  betterThan8={this.state.betterThan8}
-				/>
-			</div>
-		);
-	}
+var FooMother = React.createClass({
+  getInitialState:function(){
+    return {text:"",checkbox:false,books:[]}
+  },
+  handleFun:function(){
+    if(this.refs.te.value.trim()){
+      $.ajax({
+        url:"https://api.douban.com/v2/book/search?q="+this.refs.te.value.trim(),
+        dataType:"jsonp"
+      }).done((data)=>{
+        let _books = data.books.filter(
+            (book)=>{
+            if(!this.refs.ch.checked || (this.refs.ch.checked && book.rating.average >= 8.0)){
+                return true;
+            }
+          }).map(
+            (book) => {
+              return(
+                <li key={book.id}>
+                  <img src={book.image}/>
+                  <p>{book.title}</p>
+                  <p>{book.author}</p>
+                  <p>{book.rating.average}</p>
+                  <hr/>
+                </li>
+              );
+          });
+        this.setState({text:this.refs.te.value,checkbox:this.refs.ch.checked,books:_books});
+      }).fail(()=>{
+        console.log("ajax error!");
+      });
+    } else {
+      this.setState({text:this.refs.te.value,checkbox:this.refs.ch.checked,books:[]});
+    }
+  },
+  render:function(){
+    return (
+      <div>
+        <ImageComponent/>
+        <input type="text" placeholder="Some text ..." defaultValue={this.state.text} ref="te" onChange={_.debounce(this.handleFun,500)} />
+        <p>
+        <input type="checkbox" defaultValue={this.state.checkbox} ref="ch" onClick={_.debounce(this.handleFun,500)}/>
+        <label>８分以上</label>
+        </p>
+        <hr/>
+        <FooSon text={this.state.text} checkbox={this.state.checkbox} books={this.state.books} />
+      </div>
+    );
+  }
 });
-
 ReactDOM.render(
-  <FilterableBookListTable url="https://api.douban.com/v2/book/search" pollInterval={2000} />,
+  <FooMother />,
   document.getElementById('container')
 );
